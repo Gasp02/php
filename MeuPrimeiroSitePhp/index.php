ce3bdbfdcfe4bc7c74d6d590c8d9072dbca36d39
Gasp02 <115591780+Gasp02@users.noreply.github.com>
2023-08-01 15:06:36 -0300

    Add files via upload

--- MeuPrimeiroSitePhp/index.php
+++ MeuPrimeiroSitePhp/index.php
@@ -12,7 +12,23 @@ echo "<br>". $x;
 $txt = "Hello world!";
 $x = 5;
 $y = 10.5;
-echo "<br>".$txt."x = ".$x." y = ".$y
+echo "<br>".$txt."x = ".$x." y = ".$y;
+
+$txt = "W3Schools.com";
+ // vai dar erro o codigo abaixo;
+//echo "I love $txt!";
+ // consertando o codigo acima;
+ echo "<br>"."I love". $txt."!";
+$x = 5;
+$y = 4;
+echo "<br> o valor da soma é igual a ". $x + $y;
+var_dump($x + $y);
+echo "<br>";
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars); 
+for($i=0;$i<3;$i++){
+    echo "<br>".$cars[$i];
+}
 ?>
 
 </body>
